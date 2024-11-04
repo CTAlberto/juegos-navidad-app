7a719842a41e1c15682e26b7231539735ea8caa9
CTAlberto <145429870+CTAlberto@users.noreply.github.com>
2024-11-04 17:41:05 +0100

    Connect bbdd

---
+++ database/migrations/2024_11_04_161754_create_scores_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateScoresTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('scores', function (Blueprint $table) {
+            $table->id(); // Primary key
+            $table->integer('points')->unsigned(); // Puntos, asegurando que es un número positivo
+            $table->string('description')->nullable(); // Descripción opcional
+            $table->timestamps(); // created_at y updated_at
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('scores');
+    }
+}
