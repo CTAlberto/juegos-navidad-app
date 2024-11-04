f08199dcc0e13becc50f021b9466b7405958c276
CTAlberto <145429870+CTAlberto@users.noreply.github.com>
2024-11-04 18:24:46 +0100

    Creating some routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,32 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('createUser', function(){
+    $user = new User;
+    $user->name = 'Pollino';
+    $user->score_id = 2;
+    $user->email = 'asd@asd.vom';
+    $user->date = new DateTime();
+    $user->save();
+    return $user;
+});
+
+Route::get('users', function(){
+    return User::all();
+});
+
+Route::get('users/{id}', function($id){
+    return User::find($id);
+});
+
+Route::get('users/{id}/delete', function($id){
+    $user = User::find($id);
+    $user->delete();
+    return view();
 });
