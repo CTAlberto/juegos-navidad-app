a8fb322ebfa7793dfd3aab8e4000f027c7f1d7cb
CTAlberto <145429870+CTAlberto@users.noreply.github.com>
2024-11-06 18:36:54 +0100

    Adding id value into cell div

--- resources/views/components/gameboard.blade.php
+++ resources/views/components/gameboard.blade.php
@@ -3,10 +3,8 @@ $board = session('board');
 ?>
 <div class="grid gap-1 w-full h-full" style="grid-template-columns: repeat(<?=count($board)?>, 25%);">
         @foreach($board as $row)
-            @foreach($row as $color)
-                <div class="color-cell w-full h-full p-4" style="background-color: <?=$color['hex']?>"></div>
-
-                  
+            @foreach($row as $index => $color)
+                <div id="{{$loop->parent->index}}{{$index}}" class="color-cell w-full h-full p-4" style="background-color: <?=$color['hex']?>"></div>
             @endforeach
         @endforeach
 </div>
