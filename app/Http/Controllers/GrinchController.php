59be7d56c5dba66b0b48cf2014d3cc458c63e874
CTAlberto <145429870+CTAlberto@users.noreply.github.com>
2024-11-07 18:14:13 +0100

    Grinch moovement

--- app/Http/Controllers/GrinchController.php
+++ app/Http/Controllers/GrinchController.php
@@ -61,4 +61,56 @@ class GrinchController
     {
         //
     }
+
+    public function firstPositionGrinch($size)
+    {
+
+        $grinchPosition_y = rand(0, $size - 1);
+        $grinchPosition_x = rand(0, $size - 1);
+
+        return array('x' => $grinchPosition_x, 'y' => $grinchPosition_y);
+    }
+
+    public function moveGrinch(Request $request)
+    {
+        $x = session('grinch_position_x');
+        $y = session('grinch_position_y');
+        $size = session('size');
+        $ok = true;
+
+        do {
+            $dx = rand(-1, 1);
+            $dy = rand(-1, 1);
+
+            if ($dx === 0 && $dy === 0 || $dx !== 0 && $dy !== 0) {
+                continue;
+            }
+
+            $newX = $x + $dx;
+            $newY = $y + $dy;
+
+            if ($newX >= 0 && $newX < $size && $newY >= 0 && $newY < $size) {
+                $x = $newX;
+                $y = $newY;
+                $ok = false;
+                break;
+            }
+        } while ($ok);
+        //Añadir la a la sesión la nueva posicion del grinch
+    }
+
+    public function checkGrinch($board, $playerGuess)
+    {
+        if ($board->grinch_position_x == $playerGuess['x'] && $board->grinch_position_y == $playerGuess['y']) {
+            return true;
+        }
+        return false;
+    }
+    public function traceGrinch($board)
+    {
+        $trace = array();
+        $trace['x'] = $board->grinch_position_x;
+        $trace['y'] = $board->grinch_position_y;
+        return $trace;
+    }
 }
